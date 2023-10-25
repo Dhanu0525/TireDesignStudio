628cb580edf6ba8080d8c8fd017bc9c88781d621
Dhanu0525 <65075060+Dhanu0525@users.noreply.github.com>
2023-10-25 11:52:01 -0400

    Add files via upload

---
+++ webbbb/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Topics from "./Topics";
+import Nav from "./Nav";
+import IntroVideo from "./IntroVideo";
+
+
+export default function HomePage(){
+    return (
+        <div>
+            <Nav/>
+            <IntroVideo />
+        </div>
+    )
+}
